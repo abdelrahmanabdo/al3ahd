270b8fd60877af5453bf1f3b2323b1b9e4222aa1
abdelrahmanabdo <dev.abdelrahman1995@gmail.com>
2018-08-31 13:02:09 +0200

    push

--- resources/views/admin/subscription_messages.blade.php
+++ resources/views/admin/subscription_messages.blade.php
@@ -2,6 +2,7 @@
 
 @section('adminContent')
 
+
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
@@ -19,9 +20,9 @@
   <!-- Main content -->
   <section class="content">
     <div class="row">
-      <div class="col-md-12">
+      {{--  <div class="col-md-12">
         <a href="compose.html" class="btn btn-lg btn-primary margin-bottom">رسالة جديدة <i class="fa fa-plus" aria-hidden="true"></i></a>
-      </div>
+      </div>  --}}
       <!-- /.col -->
     </div>
     <div class="row">
@@ -30,12 +31,12 @@
           <div class="box-header with-border">
             <h3 class="box-title">الرسائل</h3>
 
-            <div class="box-tools pull-right">
+            {{--  <div class="box-tools pull-right">
               <div class="has-feedback">
                 <input type="text" class="form-control input-sm" placeholder="إبحث">
                 <span class="fa fa-search form-control-feedback"></span>
               </div>
-            </div>
+            </div>  --}}
             <!-- /.box-tools -->
           </div>
           <!-- /.box-header -->
@@ -44,13 +45,13 @@
               <!-- Check all button -->
               <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i>
               </button>
-              <div class="btn-group">
+              {{--  <div class="btn-group">
                 <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
                 <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
                 <button type="button" class="btn btn-default btn-sm"><i class="fa fa-share"></i></button>
-              </div>
+              </div>  --}}
               <!-- /.btn-group -->
-              <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>
+              {{--  <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>  --}}
               <div class="pull-right">
                 1-50/200
                 <div class="btn-group">
@@ -64,114 +65,21 @@
             <div class="table-responsive mailbox-messages">
               <table class="table table-hover table-striped">
                 <tbody>
+                @forelse($subscribed as $sub)
                 <tr>
                   <td><input type="checkbox"></td>
                   <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
                   <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
+                  <td class="mailbox-subject"><b>{{$sub->email}}
+                  <td class="mailbox-subject"><b>{{$sub->mobile_number}}
+
                   </td>
                   <td class="mailbox-attachment"></td>
-                  <td class="mailbox-date">منذ 5 دقايق</td>
                 </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                  <td class="mailbox-date">منذ 32 دقيقة</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                  <td class="mailbox-date">منذ 9 ساعات</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"></td>
-                  <td class="mailbox-date">منذ 15 ساعة</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                  <td class="mailbox-date">بالامس</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                  <td class="mailbox-date">منذ 2 يوم</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                  <td class="mailbox-date">منذ 2 يوم</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"></td>
-                  <td class="mailbox-date">منذ 2 يوم</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"></td>
-                  <td class="mailbox-date">منذ 2 يوم</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"></td>
-                  <td class="mailbox-date">منذ 2 يوم</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
-                  <td class="mailbox-date">منذ 2 يوم</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox"></td>
-                  <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-                  <td class="mailbox-name"><a href="read-mail.html">اسم المرسل</a></td>
-                  <td class="mailbox-subject"><b>موضوع الرسالة</b> - محتوى تصى لمعلومات و محتويات الرسالة ....
-                  </td>
-                  <td class="mailbox-attachment"></td>
-                  <td class="mailbox-date">1منذ 2 يوم</td>
-                </tr>
+                @empty
+                
+                <h3>لا يوجد رسائل اساءه حتي الان</h3>
+                @endforelse
                 </tbody>
               </table>
               <!-- /.table -->
@@ -179,29 +87,7 @@
             <!-- /.mail-box-messages -->
           </div>
           <!-- /.box-body -->
-          <div class="box-footer no-padding">
-            <div class="mailbox-controls">
-              <!-- Check all button -->
-              <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i>
-              </button>
-              <div class="btn-group">
-                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
-                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
-                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-share"></i></button>
-              </div>
-              <!-- /.btn-group -->
-              <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>
-              <div class="pull-right">
-                1-50/200
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
-                  <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
-                </div>
-                <!-- /.btn-group -->
-              </div>
-              <!-- /.pull-right -->
-            </div>
-          </div>
+
         </div>
         <!-- /. box -->
       </div>
